840ecad0bcd7a6d349eb6af747fe3da0875f509c
matheuseitelcampelo <matheus.e.campelo@gmail.com>
2024-08-09 06:51:02 -0300

    atualizando a class ListaEncadeada com método de remoção

--- ListaEncadeada.py
+++ ListaEncadeada.py
@@ -23,14 +23,49 @@ class ListaEncadeada:
 
         item = ItemLista(data)
 
-        #o head é apontado como próximo item
+        #apos o objeto ItemLista ser criado, ele recebe em seu
+        #atributo next o atual valor em head da ListaEncadeada
 
         item.next = self.head
-
-        #o item atual se torna o head
+    
+        #ListaEncadeada atualiza o valor de head, apontando
+        #para o ultimo objeto ItemLista criado
 
         self.head = item
 
+    def remove(self, data):
+        
+        """
+        Remove o primeiro item da lista com o valor especificado
+        """
 
+        #Verifica se o item a ser removido é o head (primeiro item da lsita)
 
+        if self.head and self.head.data == data:
+            
+            #Se o head contem o valor que procuramos, basta
+            #apontar o head para o proximo item
+            
+            self.head = self.head.next
+            print("Elemento econtrado")
+        
+        else:
+            
+            #Caso contrário, precisamos encontrar o item na lista
+            
+            before = None #'before' vai guardar o item anterior ao que estamos analisando
+            current = self.head #Começamos a navegação a partir do head
 
+            #navegue pela lista para encontrar o item com o valor especificado
+            while current and current.data != data:
+                before = current #before guarda o item atual
+                current = current.next #current avança para o proximo item
+
+            #verifica se encontramos o item
+            if current:
+                #'before.next' agora pula o item que queremos remover
+                before.next = current.next
+                print("Elemento encontrado")
+
+            else:
+                print("Elemento não encontrado")
--- processa.py
+++ processa.py
@@ -6,6 +6,14 @@ listaDeCompras = le.ListaEncadeada()
 
 #exemplo de uso
 
+listaDeCompras.insere("shampoo")
+listaDeCompras.insere("biscoito")
+listaDeCompras.insere("detergente")
+listaDeCompras.insere("abobrinha")
 listaDeCompras.insere("banana")
+
+print("Conteudo da lista:", listaDeCompras)
+
+listaDeCompras.remove("tesoura")
 
 print("Conteudo da lista:", listaDeCompras)
