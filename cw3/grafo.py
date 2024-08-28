fe6f28e2bd9919f1e15fe8f1cafe66bebe179739
matheuseitelcampelo <matheus.e.campelo@gmail.com>
2024-08-27 21:03:22 -0300

    adicionando a função de busca em profundidade Depth fist serach

--- cw3/grafo.py
+++ cw3/grafo.py
@@ -12,6 +12,24 @@ class Grafo:
         else:
             print(f"O vértice origem e ou vértice destino não existem")
 
+    
+    def dfs(self, vertice, visitados=None):
+        # Inicializa o conjunto de vértices visitados se ainda não foi passado
+        if visitados in None:
+            visitados = set() # visitados é uma variavel do tipo conjunto
+
+        # Marca o vértice atual como visitado
+            visitados.add(vertice)
+
+        #Imprime o vértice atual
+        print(vertice, end=' ')
+
+        # Percorre todos os vizinhos do vértice atual
+        for vizinho in self.grafo.get(vertice, []):
+            # Se o vizinho ainda não foi visitado, chama recursivamente dfs para ele
+            if vizinho not in visitados:
+                self.dfs(vizinho, visitados)
+
 
 # Exemplo de uso            
 g = Grafo()
